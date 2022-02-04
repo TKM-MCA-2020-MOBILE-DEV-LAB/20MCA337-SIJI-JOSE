05325991955fdc9ea0a0a22b6524aef2c85e4407
siji1999 <sijijose.sj1999@gmail.com>
2022-02-04 11:52:35 +0530

    co3

---
+++ CO3/Q3/ex1/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.explicitintent_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClick(View view) {
+        Intent i = new Intent(Intent.ACTION_VIEW);
+        i.setData(Uri.parse("https://business.instagram.com"));
+        startActivity(i);
+
+    }
+}
